0fe8b6e48913893a67db22da0caa544300925c97
Karangill2470 <karangill2470@gmail.com>
2024-10-05 22:26:49 -0400

    //

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 "use client"; // Marking this file as a Client Component
-
 import { useEffect, useState } from "react";
 import Image from "next/image";
 
@@ -15,8 +14,8 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="flex flex-col items-center justify-start min-h-screen bg-backgroundTwo p-4 bg-grey rounded-lg shadow-lg">
-      <h1 className={`text-white text-6xl ${isAnimated ? "animate-underline" : ""}`}>
+    <div className="flex flex-col items-center justify-start min-h-screen bg-background p-4 bg-grey rounded-lg shadow-lg">
+      <h1 className={`text-white text-4xl sm:text-6xl ${isAnimated ? "animate-underline" : ""}`}>
         Welcome to Planetary Seismology Explorer
       </h1>
 
@@ -29,24 +28,24 @@ export default function Home() {
           height={400}
           priority
           quality={100}
-          className="mb-4 md:mb-0" // Margin below on mobile, none on larger screens
+          className="mb-4 md:mb-0 rounded-lg shadow-md" // Margin below on mobile, none on larger screens
         />
 
         {/* h2 and p on the right side */}
         <div className="md:ml-8 text-center md:text-left">
-          <h2 className="text-white text-xl mb-2">
+          <h2 className="text-white text-xl md:text-2xl mb-2">
             Unlocking the Secrets of Seismic Events on Other Worlds
           </h2>
 
-          <p className="text-white mt-4">
+          <p className="text-white mt-4 text-base md:text-lg">
             Explore the mysteries of planetary seismology through our cutting-edge web app, designed to analyze and identify seismic quakes from historic data collected during the Apollo missions and the Mars InSight Lander. With the challenge of limited data transfer from distant planets, our innovative algorithms and manual detection methods help scientists focus on the seismic signals that matter.
           </p>
           
           {/* New div for h3 and p */}
-          <h3 className="text-white text-xl mb-2 mt-6">
+          <h3 className="text-white text-xl md:text-2xl mb-2 mt-6">
             Join the Exploration
           </h3>
-          <p className="text-white mt-4">
+          <p className="text-white mt-4 text-base md:text-lg">
             Become part of the growing community of planetary seismologists! Whether you wish to contribute your expertise, learn more about seismic analysis, or simply enjoy exploring the seismic data from other worlds, our platform welcomes you. Together, we can advance our understanding of the universe and its myriad celestial bodies.
           </p>
         </div>
